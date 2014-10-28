825059d47365742e19600a9596b11c4f72f0f0a1
SeanShubin <sshubin@cj.com>
2014-10-28 13:18:43 -0700

    Added tests

--- gui/src/main/resources/serve-from-classpath/test/tasks/create-tasks-component-test.js
+++ gui/src/main/resources/serve-from-classpath/test/tasks/create-tasks-component-test.js
@@ -9,4 +9,31 @@ define([
         var component = createTasksComponent();
         qunit.equal(component.dom.find('.new-task').attr('placeholder'), 'New Task Name', 'has proper placeholder')
     });
+    qunit.test('new task starts with focus', function () {
+        var component = createTasksComponent();
+        $('body').append(component.dom);
+        component.initialize();
+        qunit.equal(component.dom.find('.new-task').is(':focus'), true, 'New task has focus upon render');
+        component.dom.remove();
+    });
+    qunit.test('initialize fetches current state', function () {
+        var component, stubJsonOverHttp, deferredResponse;
+        deferredResponse = $.Deferred();
+        stubJsonOverHttp = function(options) {
+            qunit.equal(options.uri, 'database/tasks', 'uri');
+            qunit.equal(options.method, 'get', 'method');
+            return deferredResponse;
+        };
+        component = createTasksComponent({jsonOverHttp: stubJsonOverHttp});
+        component.initialize();
+        qunit.equal(component.dom.find('.existing-task').length, 0, 'no tasks loaded before results');
+        deferredResponse.resolve({
+            status: 200,
+            body: ['Task A', 'Task B', 'Task C']
+        });
+        qunit.equal(component.dom.find('.existing-task').length, 3, '3 tasks');
+        qunit.equal(component.dom.find('.existing-task')[0], 'Task A', 'Task A');
+        qunit.equal(component.dom.find('.existing-task')[1], 'Task B', 'Task B');
+        qunit.equal(component.dom.find('.existing-task')[2], 'Task C', 'Task C');
+    });
 });
