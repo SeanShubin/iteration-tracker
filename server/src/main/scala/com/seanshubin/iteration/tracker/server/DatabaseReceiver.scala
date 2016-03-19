3affbfbfc1a6b4d907f32a025cf827734c9788f9
SeanShubin <seanshubin@gmail.com>
2016-03-19 11:04:57 -0700

    Fixed uri issues

--- server/src/main/scala/com/seanshubin/iteration/tracker/server/DatabaseReceiver.scala
+++ server/src/main/scala/com/seanshubin/iteration/tracker/server/DatabaseReceiver.scala
@@ -6,7 +6,7 @@ import com.seanshubin.http.values.core._
 
 class DatabaseReceiver extends Receiver {
   override def receive(request: RequestValue): ResponseValue = {
-    if (request.uriString == "/database/tasks" && request.method == "GET") {
+    if (request.uri.path == "/database/tasks" && request.method == "GET") {
       val statusCode = 200
       val charset = StandardCharsets.UTF_8
       val body =
@@ -23,7 +23,7 @@ class DatabaseReceiver extends Receiver {
     } else {
       val statusCode = 404
       val charset = StandardCharsets.UTF_8
-      val body = s"Expected (get, database/tasks), but got (${request.method}, ${request.uriString})".getBytes(charset)
+      val body = s"Expected (get, database/tasks), but got (${request.method}, ${request.uri.path})".getBytes(charset)
       val contentType = ContentType("text/plain", Some(charset.name))
       val headers = Headers(Seq("Access-Control-Allow-Origin" -> "*")).setContentType(contentType)
       val responseValue = ResponseValue(statusCode, body, headers.entries)
--- server/src/main/scala/com/seanshubin/iteration/tracker/server/DatabaseRoute.scala
+++ server/src/main/scala/com/seanshubin/iteration/tracker/server/DatabaseRoute.scala
@@ -4,7 +4,7 @@ import com.seanshubin.http.values.core.{Receiver, RequestValue, Route}
 
 class DatabaseRoute(name: String, receiver: Receiver) extends Route(name, receiver) {
   override def accept(request: RequestValue): Boolean = {
-    val shouldAccept = request.uriString.startsWith("/database/")
+    val shouldAccept = request.uri.path.startsWith("/database/")
     shouldAccept
   }
 }
