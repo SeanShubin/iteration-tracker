982519d5d09b2a719c74cfb51cb7b2b54f687819
Sean Shubin <sshubin@cj.com>
2016-11-03 12:54:12 -0700

    Replaced JavaConversions with JavaConverters

--- core/src/main/scala/com/seanshubin/iteration/tracker/core/json/JsonMarshallerImpl.scala
+++ core/src/main/scala/com/seanshubin/iteration/tracker/core/json/JsonMarshallerImpl.scala
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper, Ser
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.seanshubin.http.values.core.StringUtil
 
-import scala.collection.JavaConversions
+import scala.collection.JavaConverters._
 
 class JsonMarshallerImpl extends JsonMarshaller {
   private val mapper = new ObjectMapper()
@@ -38,7 +38,7 @@ class JsonMarshallerImpl extends JsonMarshaller {
   override def fromJsonArray[T](json: String, theElementClass: Class[T]): Seq[T] = {
     val collectionType = mapper.getTypeFactory.constructCollectionType(classOf[java.util.List[T]], theElementClass)
     val myObjects: java.util.List[T] = mapper.readValue(json, collectionType)
-    JavaConversions.collectionAsScalaIterable(myObjects).toSeq
+    myObjects.asScala
   }
 
   override def normalize(json: String): String = {
